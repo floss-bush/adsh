5764c70e9bcce103a86303ffb8efbf6a0874dec5
Gjergj Sheldija <gjergj.sheldija@gmail.com>
2012-12-04 20:32:30 +0100

    item view

--- themes/bush/items/show.php
+++ themes/bush/items/show.php
@@ -1,143 +1,167 @@
-<?php 
-    head(array('title' => item('Dublin Core', 'Title'), 'bodyid'=>'items','bodyclass' => 'show')); 
+<?php
+    head(array('title' => item('Dublin Core', 'Title'), 'bodyid'=>'items','bodyclass' => 'show'));
     $item = $this->item;
     ?>
-<div id="primary">
-    <div class="row">
-        <div class="span12">
-            <div class="pagination pagination-centered">
-                <ul>
-                    <li id="previous-item" class="previous"><?php echo link_to_previous_item(); ?></li>
-                </ul>
-                <ul>
-                    <li id="next-item" class="next"><?php echo link_to_next_item(); ?></li>
-                </ul>
+<div class="splashimage">
+</div>
+
+<div class="subtitle">
+    <h1>Explore the Collection</h1>
+</div>
+
+    <div class="content">
+        <div class="container">
+
+
+            <div class="secondary-nav top">
+                <span class="previous">
+                    <?php echo link_to_previous_item('&laquo; Previous Item'); ?>
+                </span>
+                <span class="next">
+                    <?php echo link_to_next_item('Next Item &raquo;'); ?>
+                </span>
             </div>
-        </div>
+    <div id="primary">
+
+        <div class="row">
+
+                <div class="span1 noprint" style="text-align: center;">
+
+    <!-- AddThis Button BEGIN -->
+    <div class="addthis_toolbox addthis_32x32_style" style="margin-left: 20px;">
+    <a class="addthis_button_facebook"></a>
+    <a class="addthis_button_twitter"></a>
+    <a class="addthis_button_linkedin"></a>
+    <a class="addthis_button_compact"></a>
     </div>
-    <div class="row">
-        <div class="span12">
-            <div class="page-header"><h1><?php echo item('Dublin Core', 'Title'); ?></h1></div>
-            <?php echo flash(); ?>
+    <script type="text/javascript">
+    var addthis_share =
+    {
+        title: "Arkivat Shkoder | Database i materialit arkivor te bibliotekave te Shkodres",
+        url_transforms : {
+             shorten: {
+                 twitter: 'bitly'
+            }
+        },
+        shorteners : {
+             bitly : {}
+        }
+    };
+    </script>
+    <script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#pubid=gjergjsheldija"></script>
+    <!-- AddThis Button END -->
+
+                &nbsp;
         </div>
+
+        <div class="span11">
+            <div class="span6" style="float: right;">
+                <ul class="thumbnails">
+                <li class="span6">
+                    <?php echo
+                        display_files_for_item(
+                            array('imageSize'=>'thumbnail','linkToFile'=>true,'linkToMetadata'=>false, 'linkAttributes' => array('class' => 'thumbnail fancybox-zoomit fancybox.iframe', 'type'=>'iframe', 'rel'=>'images')),
+                            array('id'=>'item-image'), //wrapperAttributes
+                            null
+                        );
+                    ?>
+                </li>
+                </ul>
+            </div>
+
+        <?php echo flash(); ?>
+        <h2><?php echo item('Dublin Core', 'Title') . item('Dublin Core','Identifier'); ?> </h2>
+
+    <div class="element-set">
+        <h3><?php echo __('Description'); ?></h3>
+        <?php if ($itemDescription = item('Dublin Core','Description')): ?>
+            <p><?php echo $itemDescription; ?></p>
+        <?php else: ?>
+            <p><strong>Sorry!</strong> No description recorded.</p>
+        <?php endif; ?>
     </div>
-    <div class="row">
-        <div class="span6">
-            <!-- Item Description -->
-            <div class="row">
-                <div class="span6">
-                    <?php if ($itemDescription = item('Dublin Core','Description')): ?>
-                        <p class="lead"><?php echo $itemDescription; ?></p>
-                    <?php else: ?>
-                        <h4>Description</h4>
-                        <p class="alert"><strong>Sorry!</strong> No description recorded.</p>
-                    <?php endif; ?>
-                </div>
-            </div>
-            
-            <!-- Item Collection Information (if available) -->
-            <?php if (item_belongs_to_collection()): ?>
-            <div class="row"><div class="span6">
-                <div id="collection" class="element">
-                    <h4 style="display:inline"><?php echo __('Collection'); ?>: </h4>
-                    <h4 style="display:inline"><?php echo link_to_collection_for_item(); ?></h4>
-                </div>
-            </div></div>
-            <?php endif; ?>
-            
-            <div class="row"><div class="span6"><hr /></div></div>
-            
-            <div class="row">
-                <div class="span2">
-                <!-- Item Date Information -->    
-                    <h4><i class="icon-calendar icon-large"></i> Date: </h5>
-                    <?php if ($itemDate = item('Dublin Core','Date')): // TODO: create a date format function...?>
-                        <div class="lead"><?php echo $itemDate; ?></div>
-                    <?php else: ?>
-                        <div class="lead">None recorded.</div>
-                    <?php endif; ?>
-                </div>
-                <div class="span2">
-                <!-- Item Creator Information -->
-                    <h4><i class="icon-user icon-large"></i> Author: </h4>
-                    <div class="lead">
-                    <?php if ($itemCreator = item('Dublin Core','Creator')): // TODO: create a date format function...?>
-                        <?php echo $itemCreator; ?>
-                    <?php else: ?>
-                        None recorded.
-                    <?php endif; ?>
-                    </div>
-                </div>
-                <div class="span2">
-                <!-- Item Recipient Information (if available) -->
-                    <?php if ($itemRecipient = item('Item Type Metadata','Recipient')): // TODO: create a date format function...?>
-                        <h4><i class="icon-envelope icon-large"></i> Recipient: </h4>
-                        <div class="lead">
-                            <?php echo $itemRecipient; ?>
-                        </div>
-                    <?php endif; ?>
-                </div>
-            </div>
-            
-            <!-- If the item belongs to a collection, the following creates a link to that collection. -->
-            
-                
-            <!-- The following prints a list of all tags associated with the item -->
-            <div class="row">
-                <div class="span6">
-                    <hr />
-                    <h4><i class="icon-tags icon-large"></i> Tags</h4>
-                    <div class="tags well well-small">
-                        <div><strong>Current Tags</strong></div>
-                        <?php if (item_tags_as_string() != null) {
+
+    <div id="item-tags" class="element">
+        <h3><?php echo __('Keywords'); ?></h3>
+        <?php if (item_tags_as_string() != null) {
                             echo item_tags_as_string(); }
                             else {
-                            echo 'No tags recorded for this item.'; 
+                            echo 'No tags recorded for this item.';
                             }
                         ?>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="span6">
-                    <hr />
-                    <!-- The following prints a citation for this item. -->
-                    <h4><i class="icon-retweet icon-large"></i> <?php echo __('Citation'); ?></h4>
-                    <div class="element-text"><?php echo item_citation($item); ?></div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="span6">
-                    <?php echo plugin_append_to_items_show(); ?>
-                </div>
-            </div>
-        </div>
-        <!-- The following returns all of the files associated with an item. -->
-        <div id="itemfiles" class="span6">
-            <!-- <h3><?php echo __('Files'); ?></h3> -->
-            <p class="lead" style="text-align:center;">Item Identification #: <?php echo item('Dublin Core','Identifier'); ?></p>
-           
-            <div class="element-text"><?php echo display_files_for_item(
-                array('imageSize'=>'fullsize','linkToFile'=>true,'linkToMetadata'=>false),//options
-                array('class'=>'file-image'), //wrapperAttributes
-                null); 
-        ?></div>
+    </div>
+
+    <div id="item-collection" class="element">
+           <h3><?php echo __('Collection'); ?></h3>
+        <div class="element-text">
+            <?php echo link_to_collection_for_item(); ?>
         </div>
     </div>
-    <div class="row">
-        <div class="span12">
-            <div class="pagination pagination-centered">
-                <ul>
-                    <li id="previous-item" class="previous"><?php echo link_to_previous_item(); ?></li>
-                </ul>
-                <ul>
-                    <li id="next-item" class="next"><?php echo link_to_next_item(); ?></li>
-                </ul>
-            </div>
+
+    <div id="date-collection" class="element">
+           <h3><?php echo __('Date'); ?></h3>
+        <div class="element-text">
+            <?php echo item('Dublin Core','Date'); ?>
         </div>
     </div>
-        
+
+    <div id="creator-collection" class="element">
+           <h3><?php echo __('Author'); ?></h3>
+        <div class="element-text">
+            <?php echo item('Dublin Core','Creator'); ?>
+        </div>
+    </div>
+
+    <div id="item-citation" class="element" style="clear: both;">
+        <h3><?php echo __('Citation'); ?></h3>
+        <div class="element-text">
+            <?php echo item_citation($item); ?>
+        </div>
+    </div>
+
+</div> <!-- /span11 -->
+
+
+</div> <!-- /row -->
+
+</div> <!-- /#primary -->
+
+    <div class="secondary-nav bottom">
+        <span class="previous">
+            <?php echo link_to_previous_item('&laquo; Previous Item'); ?></span>
+        <span class="next">
+            <?php echo link_to_next_item('Next Item &raquo;'); ?></span>
+    </div>
+
+
+        </div> <!-- /container -->
+
+    </div> <!-- /content -->
+
+
 </div>
 <!-- end primary -->
+<script type="text/javascript">
+jQuery(document).ready(function() {
 
+    if (!jQuery.browser.mobile)
+    {
+        jQuery(".fancybox-zoomit").fancybox({
+            width       : '90%',
+            height      : '90%',
+            autoSize    : true,
+            closeClick  : false,
+            preload     : 0,
+            loop        : false,
+            iframe      : { preload : false },
+            openEffect  : 'none',
+            closeEffect : 'none',
+            nextEffect  : 'none',
+            prevEffect  : 'none',
+            padding     : 0,
+            margin      : [20, 60, 20, 60] // Increase left/right margin
+        });
+    }
+});
+</script>
 <?php foot(); ?>
