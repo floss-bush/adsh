c317dc4b10036ca0bff7d079e1c57a0d3e003e09
Gjergj Sheldija <gjergj.sheldija@gmail.com>
2012-12-05 14:53:47 +0100

    pagination control

--- themes/bush/common/pagination_control.php
+++ themes/bush/common/pagination_control.php
@@ -1,43 +1,45 @@
 <?php if ($this->pageCount > 1): ?>
-<ul class="pagination_list">
-    
+<div id="pagination-top" class="pagination">
+<table class="pagination_list">
+    <tr>
+
     <?php if ($this->first != $this->current): ?>
-        <!-- First page link --> 
-        <li>
-            <a href="<?php echo html_escape($this->url(array('page' => $this->first), null, $_GET)); ?>"><?php echo __('First'); ?></a>
-        </li>
+        <!-- First page link -->
+        <td class="pagination_first">
+            <a href="<?php echo html_escape($this->url(array('page' => $this->first), null, $_GET)); ?>"><?php echo __('&laquo; First'); ?></a>
+        </td>
     <?php endif; ?>
-</ul>
-<ul>   
+
     <?php if (isset($this->previous)): ?>
-    <!-- Previous page link --> 
-    <li class="pagination_previous">
-    <a href="<?php echo html_escape($this->url(array('page' => $this->previous), null, $_GET)); ?>"><?php echo __('Prev'); ?></a>
-    </li>
+    <!-- Previous page link -->
+    <td class="pagination_previous">
+        <a href="<?php echo html_escape($this->url(array('page' => $this->previous), null, $_GET)); ?>"><?php echo __('&laquo; Prev'); ?></a>
+    </td>
     <?php endif; ?>
-    
+
     <!-- Numbered page links -->
-    <?php foreach ($this->pagesInRange as $page): ?> 
+    <?php foreach ($this->pagesInRange as $page): ?>
         <?php if ($page != $this->current): ?>
-            <li><a href="<?php echo html_escape($this->url(array('page' => $page), null, $_GET)); ?>"><?php echo $page; ?></a></li>
+            <td class="pagination_range"><a href="<?php echo html_escape($this->url(array('page' => $page), null, $_GET)); ?>"><?php echo $page; ?></a></td>
         <?php else: ?>
-            <li class="current active"><span><?php echo $page; ?></span></li>
+            <td class="pagination_current"><?php echo $page; ?></td>
         <?php endif; ?>
     <?php endforeach; ?>
-    
-    <?php if (isset($this->next)): ?> 
+
+    <?php if (isset($this->next)): ?>
     <!-- Next page link -->
-    <li class="pagination_next">
-    <a href="<?php echo html_escape($this->url(array('page' => $this->next), null, $_GET)); ?>"><?php echo __('Next'); ?></a>
-    </li>
+    <td class="pagination_next">
+        <a href="<?php echo html_escape($this->url(array('page' => $this->next), null, $_GET)); ?>"><?php echo __('Next &raquo;'); ?></a>
+    </td>
     <?php endif; ?>
-</ul>
-<ul>
+
     <?php if ($this->last != $this->current): ?>
-    <!-- Last page link --> 
-    <li class="pagination_last">
-    <a href="<?php echo html_escape($this->url(array('page' => $this->last), null, $_GET)); ?>"><?php echo __('Last'); ?></a>
-    </li>
+    <!-- Last page link -->
+    <td class="pagination_last">
+        <a href="<?php echo html_escape($this->url(array('page' => $this->last), null, $_GET)); ?>"><?php echo __('Last &raquo;'); ?></a>
+    </td>
     <?php endif; ?>
-</ul>
+    </tr>
+</table>
+</div>
 <?php endif; ?>
