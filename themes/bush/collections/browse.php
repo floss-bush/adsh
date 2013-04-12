253b9e99e97dddd0441f6e1239eae5bddaf75762
Gjergj Sheldija <gjergj.sheldija@gmail.com>
2013-04-12 11:02:31 +0200

    fixed collection browsing

--- themes/bush/collections/browse.php
+++ themes/bush/collections/browse.php
@@ -18,14 +18,18 @@
 
             <?php while (loop_collections()): ?>
             <div class="collection">
-                    <h2><?php echo link_to_collection(); ?></h2>
+                <h2><?php echo link_to_collection(); ?></h2>
 
-                    <div class="element">
-                        <h3>Description</h3>
-                        </div><?php echo nls2p(collection('Description', array('snippet'=>150))); ?></div>
+                <div class="element">
+                    <h3>Description</h3>
+                    <div>
+                        <?php echo nls2p(collection('Description', array('snippet'=>150))); ?>
                     </div>
+                </div>
 
-                <p class="view-items-link"><?php echo link_to_browse_items('View the items in this collection', array('collection' => collection('id'))); ?></p>
+                <p class="view-items-link">
+                    <?php echo link_to_browse_items('View the items in this collection', array('collection' => collection('id'))); ?>
+                </p>
             </div>
             <!-- end class="collection" -->
             <?php endwhile; ?>
